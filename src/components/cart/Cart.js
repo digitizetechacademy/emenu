59029cda8db84a1895ad83b77ab333adb7c0b868
Digitize Tech Academy <digitizetechacademy@gmail.com>
2025-09-10 03:44:05 +0000

    entry Cutomer name should be visible.   (_for element input_)

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -118,7 +118,7 @@ const Cart = (props) => {
 
   const cartModalContent = (
     <React.Fragment>
-      <ul className={`${classes["cart-items"]} max-h-[50vh] overflow-y-auto`}>{cartItemsRendered}</ul>
+      <ul className={`${classes["cart-items"]} max-h-[50vh] overflow-y-auto pr-4`}>{cartItemsRendered}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>₹{totalAmount}</span>
